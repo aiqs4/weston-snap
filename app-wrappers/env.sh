e62ba492c61b95b8c16fe8c18262797ce8826d67
Sascha Dewald <sascha.dewald@gmail.com>
2019-12-09 01:11:23 +0100

    use extension

--- app-wrappers/env.sh
+++ app-wrappers/env.sh
@@ -1,18 +1,17 @@
 #!/bin/sh -e
 
-
 WESTON_MODULE_MAP=""
 WESTON_MODULE_MAP="${WESTON_MODULE_MAP};weston-desktop-shell=${SNAP}/usr/libexec/weston-desktop-shell"
 
 ## FIXME: TODO: ivi is configureable !
 WESTON_MODULE_MAP="${WESTON_MODULE_MAP};weston-ivi-shell-user-interface=${SNAP}/usr/libexec/weston-ivi-shell-user-interface"
 WESTON_MODULE_MAP="${WESTON_MODULE_MAP};weston-keyboard=${SNAP}/usr/libexec/weston-keyboard"
+
 ## FIXME: TODO: weston-screenshooter is also configurable !
 WESTON_MODULE_MAP="${WESTON_MODULE_MAP};weston-screenshooter=${SNAP}/usr/libexec/weston-screenshooter"
 WESTON_MODULE_MAP="${WESTON_MODULE_MAP};weston-simple-im=${SNAP}/usr/libexec/weston-simple-im"
 
 WESTON_MODULE_MAP="${WESTON_MODULE_MAP};desktop-shell.so:${SNAP}/usr/lib/${SNAPCRAFT_ARCH_TRIPLET}/weston/desktop-shell.so"
-
 
 WESTON_MODULE_MAP="${WESTON_MODULE_MAP};fbdev-backend.so=${SNAP}/usr/lib/${SNAPCRAFT_ARCH_TRIPLET}/libweston-7/fbdev-backend.so"
 WESTON_MODULE_MAP="${WESTON_MODULE_MAP};gl-renderer.so=${SNAP}/usr/lib/${SNAPCRAFT_ARCH_TRIPLET}/libweston-7/gl-renderer.so"
@@ -23,5 +22,4 @@ WESTON_MODULE_MAP="${WESTON_MODULE_MAP};xwayland.so=${SNAP}/usr/lib/${SNAPCRAFT_
 WESTON_MODULE_MAP="${WESTON_MODULE_MAP};drm-backend.so=${SNAP}/usr/lib/${SNAPCRAFT_ARCH_TRIPLET}/libweston-7/drm-backend.so"
 
 export WESTON_MODULE_MAP
-
 
